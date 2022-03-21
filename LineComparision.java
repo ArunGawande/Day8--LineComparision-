e302bce7e8cd11cf9ada1f4fc3a9639ffa654320
Arun <arungawande1995@gmail.com>
2022-03-21 16:34:42 +0530

    Added:UC-2

--- LineComparision.java
+++ LineComparision.java
@@ -6,7 +6,7 @@ public class LineComparision
 {
     public static void main(String args[]) {
         //Declaration
-        double x1, x2, y1, y2, LengthofLine;
+        double x1, x2, y1, y2, LengthofLine, LengthofLine2;
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the value of x1");
@@ -25,5 +25,26 @@ public class LineComparision
         LengthofLine = Math.sqrt(  (Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)) );
         System.out.println(LengthofLine);
 
+        System.out.println("Enter the value of x1");
+        //taking input from user
+        x1 = scan.nextDouble();
+        System.out.println("Enter the value of x2");
+        //taking input from user
+        x2 = scan.nextDouble();
+        System.out.println("Enter the value of y1");
+        //taking input from user
+        y1 = scan.nextDouble();
+        System.out.println("Enter the value of y2");
+        //taking input from user
+        y2 = scan.nextDouble();
+
+        LengthofLine2 = Math.sqrt(  (Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)) );
+        System.out.println(LengthofLine);
+
+
+        String s1,s2;
+        s1=Double.toString(LengthofLine);
+        s2=Double.toString(LengthofLine2);
+        System.out.println("Equality of two length is: " + s1.equals(s2));
     }
 }
